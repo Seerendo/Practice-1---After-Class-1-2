afde11c028e95680f8784129389ec634ffd5b1ee
Seerendo <70487872+Seerendo@users.noreply.github.com>
2022-09-30 09:21:53 -0500

    Practice css

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Users } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
 })
-export class TableComponent implements OnInit {
+export class TableComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+   public tables: Users[] = [
+    {
+      email: 'a',
+      phone: 12345,
+      first_name: 'A',
+      last_name: 'Aa',
+      age: 1,
+    },
+    {
+      email: 'a',
+      phone: 12345,
+      first_name: 'A',
+      last_name: 'Aa',
+      age: 1,
+    },
+  ];
 }
