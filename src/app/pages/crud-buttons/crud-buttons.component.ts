17b8e9657befad20662f9176df270bf19f3bb07a
Seerendo <70487872+Seerendo@users.noreply.github.com>
2022-09-30 15:03:59 -0500

    .

---
+++ src/app/pages/crud-buttons/crud-buttons.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-buttons',
+  templateUrl: './crud-buttons.component.html',
+  styleUrls: ['./crud-buttons.component.css'],
+})
+export class CrudButtonsComponent {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AddUserComponent } from './add-user/add-user.component';
+import { CrudButtonsComponent } from './crud-buttons/crud-buttons.component';
 import { HomeComponent } from './home/home.component';
 import { TableComponent } from './table/table.component';
 
 @NgModule({
-  declarations: [HomeComponent, TableComponent, AddUserComponent],
+  declarations: [
+    HomeComponent,
+    TableComponent,
+    AddUserComponent,
+    CrudButtonsComponent,
+  ],
   exports: [HomeComponent],
   imports: [CommonModule],
 })
